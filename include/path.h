5734c27d3d7891f1235e46fe8373d40bd34ba927
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-10-26 11:39:58 +0000

    Fix some more docs, add images, correct some doxygen commands, etc.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/branches/releases/1.6@2753 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/path.h
+++ include/path.h
@@ -12,6 +12,8 @@ namespace irr
 namespace io
 {
 
+//! Type used for all file system things.
+/** This type will transparently handle different file system encodings. */
 typedef core::string<fschar_t> path;
 
 } // io
